b8ba4df9ed6868779111526e1c984ff7520768e1
ImGreatest <ImGreatest@users.noreply.github.com>
2024-03-21 23:14:29 +0300

    Fix: rating.module.ts

--- src/app/pages/rating/rating.module.ts
+++ src/app/pages/rating/rating.module.ts
@@ -1,25 +1,37 @@
 import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
-import {TUI_SANITIZER, TuiModeModule, TuiTextfieldControllerModule} from "@taiga-ui/core";
+import {
+  TUI_SANITIZER,
+  TuiButtonModule,
+  TuiLinkModule,
+  TuiModeModule,
+  TuiTextfieldControllerModule
+} from "@taiga-ui/core";
 import {NgDompurifySanitizer} from "@tinkoff/ng-dompurify";
 import {RatingComponent} from "./rating.component";
 import {RouterModule} from "@angular/router";
-import {TuiInputModule} from "@taiga-ui/kit";
+import {TuiInputModule, TuiPaginationModule, TuiTagModule} from "@taiga-ui/kit";
 import {TuiTableModule} from "@taiga-ui/addon-table";
+import {TuiLetModule} from "@taiga-ui/cdk";
 
 
 
 @NgModule({
-  imports: [
-    CommonModule,
-    RouterModule.forChild([
-      {path: '', component: RatingComponent}
-    ]),
-    TuiInputModule,
-    TuiTextfieldControllerModule,
-    TuiModeModule,
-    TuiTableModule,
-  ],
+    imports: [
+        CommonModule,
+        RouterModule.forChild([
+            {path: '', component: RatingComponent}
+        ]),
+        TuiInputModule,
+        TuiTextfieldControllerModule,
+        TuiModeModule,
+        TuiTableModule,
+        TuiTagModule,
+        TuiButtonModule,
+        TuiLinkModule,
+        TuiLetModule,
+        TuiPaginationModule,
+    ],
   providers: [
     {
       provide: TUI_SANITIZER,
