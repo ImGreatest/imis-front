b6cb668d63ee3693025400ad8bd6faba4ca8132d
mokhovIvan <mokhov@synaptik.ru>
2024-03-21 15:27:37 +0000

    Layout rating page

--- src/app/pages/rating/rating.component.ts
+++ src/app/pages/rating/rating.component.ts
@@ -1,8 +1,55 @@
 import { Component } from '@angular/core';
+import {TuiDay} from "@taiga-ui/cdk";
+
+
+
+interface User {
+  readonly dob: TuiDay;
+  readonly name: string;
+}
+
+const TODAY = TuiDay.currentLocal();
+const FIRST = [
+  'John',
+  'Jane',
+  'Jack',
+  'Jill',
+  'James',
+  'Joan',
+  'Jim',
+  'Julia',
+  'Joe',
+  'Julia',
+];
+
+const LAST = [
+  'Smith',
+  'West',
+  'Brown',
+  'Jones',
+  'Davis',
+  'Miller',
+  'Johnson',
+  'Jackson',
+  'Williams',
+  'Wilson',
+];
+
+const DATA: readonly User[] = Array.from({length: 20}, () => ({
+  name: `${LAST[Math.floor(Math.random() * 10)]}, ${
+    FIRST[Math.floor(Math.random() * 10)]
+  }`,
+  dob: TODAY.append({day: -Math.floor(Math.random() * 4000) - 7500}),
+}));
+
+
 
 @Component({
   selector: 'app-rating',
   templateUrl: './rating.component.html',
   styleUrl: './rating.component.less'
 })
-export class RatingComponent {}
+export class RatingComponent {
+  protected columns = ['ФИО', "e-mail", "Курс", 'Направление', 'Группа', ''];
+  readonly data = DATA;
+}
--- src/app/pages/rating/rating.module.ts
+++ src/app/pages/rating/rating.module.ts
@@ -1,9 +1,11 @@
 import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
-import {TUI_SANITIZER} from "@taiga-ui/core";
+import {TUI_SANITIZER, TuiModeModule, TuiTextfieldControllerModule} from "@taiga-ui/core";
 import {NgDompurifySanitizer} from "@tinkoff/ng-dompurify";
 import {RatingComponent} from "./rating.component";
 import {RouterModule} from "@angular/router";
+import {TuiInputModule} from "@taiga-ui/kit";
+import {TuiTableModule} from "@taiga-ui/addon-table";
 
 
 
@@ -13,6 +15,10 @@ import {RouterModule} from "@angular/router";
     RouterModule.forChild([
       {path: '', component: RatingComponent}
     ]),
+    TuiInputModule,
+    TuiTextfieldControllerModule,
+    TuiModeModule,
+    TuiTableModule,
   ],
   providers: [
     {
