d37b32ae0c82b7aa10cd4152342ba9e0e719e8c7
Ermac <tulupov.valeriy@mail.ru>
2024-04-25 11:31:01 +0300

    fixed

--- src/app/component/project-card/project-card.module.ts
+++ src/app/component/project-card/project-card.module.ts
@@ -1,72 +1,73 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
+import {NgModule} from "@angular/core";
+import {CommonModule} from "@angular/common";
 import {
-  TuiAmountPipeModule,
-  TuiAutoColorModule,
-  TuiAvatarModule, TuiAvatarOutlineModule, TuiAvatarStackModule,
-  TuiCardModule, TuiCellModule, TuiFallbackSrcModule,
-  TuiHeaderModule, TuiInitialsModule,
-  TuiSurfaceModule,
-  TuiTitleModule
-} from "@taiga-ui/experimental";
-import {
-  TUI_SANITIZER,
-  TuiDataListModule,
-  TuiDropdownModule,
-  TuiHostedDropdownModule,
-  TuiSvgModule, TuiTextfieldControllerModule
-} from "@taiga-ui/core";
-import {
-  TuiDataListDropdownManagerModule,
-  TuiDataListWrapperModule,
-  TuiItemsWithMoreModule, TuiSelectModule,
-  TuiTagModule
-} from "@taiga-ui/kit";
-import { TuiActiveZoneModule, TuiRepeatTimesModule } from "@taiga-ui/cdk";
-import { ProjectCardComponent } from "./project-card.component";
-import { RouterLink } from "@angular/router";
-import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
-import { FormsModule } from "@angular/forms";
-import { TuiDropdownMobileModule } from "@taiga-ui/addon-mobile";
-
-@NgModule({
-  imports: [
-    CommonModule,
-    TuiCardModule,
-    TuiHeaderModule,
-    TuiSurfaceModule,
-    TuiHostedDropdownModule,
-    TuiTitleModule,
-    TuiItemsWithMoreModule,
-    TuiTagModule,
+    TuiAmountPipeModule,
+    TuiAutoColorModule,
     TuiAvatarModule,
     TuiAvatarOutlineModule,
-    TuiDropdownModule,
-    TuiSvgModule,
     TuiAvatarStackModule,
-    TuiInitialsModule,
-    TuiAutoColorModule,
-    TuiDataListModule,
-    TuiActiveZoneModule,
+    TuiCardModule,
     TuiCellModule,
-    TuiDataListDropdownManagerModule,
-    TuiRepeatTimesModule,
-    RouterLink,
     TuiFallbackSrcModule,
-    TuiDataListWrapperModule,
-    TuiSelectModule,
-    FormsModule,
-    TuiTextfieldControllerModule,
-    TuiDropdownMobileModule,
-    TuiAmountPipeModule
-  ],
-  providers: [
-    {
-      provide: TUI_SANITIZER,
-      useClass: NgDompurifySanitizer
-    },
-  ],
-  declarations: [ProjectCardComponent],
-  exports: [ProjectCardComponent],
+    TuiHeaderModule,
+    TuiInitialsModule,
+    TuiSurfaceModule,
+    TuiTitleModule
+} from "@taiga-ui/experimental";
+import {
+    TUI_SANITIZER,
+    TuiDataListModule,
+    TuiDropdownModule,
+    TuiHostedDropdownModule,
+    TuiSvgModule,
+    TuiTextfieldControllerModule
+} from "@taiga-ui/core";
+import {TuiDataListDropdownManagerModule, TuiDataListWrapperModule, TuiItemsWithMoreModule, TuiSelectModule, TuiTagModule} from "@taiga-ui/kit";
+import {TuiActiveZoneModule, TuiRepeatTimesModule} from "@taiga-ui/cdk";
+import {ProjectCardComponent} from "./project-card.component";
+import {RouterLink} from "@angular/router";
+import {NgDompurifySanitizer} from "@tinkoff/ng-dompurify";
+import {FormsModule} from "@angular/forms";
+import {TuiDropdownMobileModule} from "@taiga-ui/addon-mobile";
+
+@NgModule({
+    imports: [
+        CommonModule,
+        TuiCardModule,
+        TuiHeaderModule,
+        TuiSurfaceModule,
+        TuiHostedDropdownModule,
+        TuiTitleModule,
+        TuiItemsWithMoreModule,
+        TuiTagModule,
+        TuiAvatarModule,
+        TuiAvatarOutlineModule,
+        TuiDropdownModule,
+        TuiSvgModule,
+        TuiAvatarStackModule,
+        TuiInitialsModule,
+        TuiAutoColorModule,
+        TuiDataListModule,
+        TuiActiveZoneModule,
+        TuiCellModule,
+        TuiDataListDropdownManagerModule,
+        TuiRepeatTimesModule,
+        RouterLink,
+        TuiFallbackSrcModule,
+        TuiDataListWrapperModule,
+        TuiSelectModule,
+        FormsModule,
+        TuiTextfieldControllerModule,
+        TuiDropdownMobileModule,
+        TuiAmountPipeModule
+    ],
+    providers: [
+        {
+            provide: TUI_SANITIZER,
+            useClass: NgDompurifySanitizer
+        }
+    ],
+    declarations: [ProjectCardComponent],
+    exports: [ProjectCardComponent]
 })
 export class ProjectCardModule {}
