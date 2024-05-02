2be9ee7a361b0db005268869e787b3e5ce045f20
Ermac <tulupov.valeriy@mail.ru>
2024-05-02 14:36:37 +0300

    env

--- src/enviroments/enviroments.ts
+++ src/enviroments/enviroments.ts
@@ -5,8 +5,8 @@ export const environment = {
   //apiMobileUrl: 'http://localhost:3002/api',
   apiMobileUrl: `http://62.217.180.64:3007/api`,
   // apiUserUrl: 'http://localhost:3003/api',
-  // apiUserUrl: `http://62.217.180.64:3004/api`,
-  apiUserUrl: 'http://localhost:3000/api',
+  apiUserUrl: `http://62.217.180.64:3004/api`,
+  // apiUserUrl: 'http://localhost:3000/api',
   // apiEmployerUrl: 'http://localhost:3000/api',
   apiEmployerUrl: `http://62.217.180.64:3006/api`,
 };
