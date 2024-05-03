f6b0a7937474b7f20d1627ab08913c91faa40454
270rq <tulupov.valeriy@mail.ru>
2024-05-03 12:27:03 +0300

    success modal

---
+++ src/app/pages/success/create-update-success/create-update-success.component.ts
@@ -0,0 +1,26 @@
+import { Component, ChangeDetectionStrategy, OnInit, Inject } from "@angular/core";
+import { ISuccessModalData } from "@interfaces";
+import { TuiDialogContext } from "@taiga-ui/core";
+import { POLYMORPHEUS_CONTEXT } from '@tinkoff/ng-polymorpheus';
+
+@Component({
+    selector: 'create-update-success',
+    templateUrl: './create-update-success.component.html',
+    styleUrls: ['./create-update-success.component.less'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+  })
+  export class CreateUpdateSuccessComponent implements OnInit {
+    constructor(
+      @Inject(POLYMORPHEUS_CONTEXT)
+      private readonly context: TuiDialogContext<any, ISuccessModalData>,
+    ){}
+    ngOnInit(): void {
+      this.successId && this._fetchData(this.orderId);
+    }
+
+  get successId(): number {
+    return this.context.data.successId;
+  }
+
+    
+  }
