3d5cdfbbe2967f3254562e5958ba178adc827409
ImGreatest <cmayll@mail.ru>
2024-05-03 12:52:20 +0300

    email crud

--- src/app/common/services/api/email.service.ts
+++ src/app/common/services/api/email.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { AppService } from "@services";
+import { IActionConfirm, IMessageHtml, IMessageText } from "@interfaces";
+import { Observable } from "rxjs";
 
-@Injectable()
+@Injectable({ providedIn: 'root' })
 export class EmailService {
   constructor(
     public http: HttpClient,
@@ -10,10 +12,15 @@ export class EmailService {
   ) {}
 
   get url(): string {
-    return this.appService.apiUserUrl + 'user';
+    return this.appService.apiUserUrl + 'email-service';
   }
 
-  sentMail(to: string) {
-    return this.http.get(`${this.url}/`)
+  sentMessage(message: IMessageText) {
+    console.log("sentMessage APi method", message);
+    return this.http.post(`http://localhost:6000/api/email-service/sent-text-message`, message);
+  }
+
+  sentConfirmActionMessage(message: IMessageHtml): Observable<IActionConfirm> {
+    return this.http.post<IActionConfirm>(`${this.url}/sent-confirm-message`, message);
   }
 }
