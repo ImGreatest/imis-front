befc69f7291011e8260de364c1ead4cfba0c9b1a
ImGreatest <ImGreatest@users.noreply.github.com>
2024-05-04 16:02:09 +0300

    fix tabs

--- src/app/common/services/api/role.service.ts
+++ src/app/common/services/api/role.service.ts
@@ -9,25 +9,23 @@ import { PageRes } from '@interfaces';
 export class RoleService {
     constructor(public http : HttpClient) {}
 
-    create(role : IUpdateCreateRole) : Observable < IRole > {
-        return this.http.post < IRole > (`${environment.apiRatingUrl}/role`, role);
+    create(role : IUpdateCreateRole): Observable <IRole> {
+      return this.http.post<IRole>(`${environment.apiRatingUrl}/role`, role);
     }
+
     getPage(limit : number, page : number, direction : -1 | 1, name : string) : Observable < PageRes < IRole >> {
-        return this.http.get < PageRes < IRole >> (`${environment.apiRatingUrl}/role/page-${page}?limit=${limit}&direction=${direction}&name=${name}`)
+      return this.http.get<PageRes<IRole>>(`${environment.apiRatingUrl}/role/page-${page}?limit=${limit}&direction=${direction}&name=${name}`);
     }
+
     getRoleAssert() {
-        return this.http.get < IRoleAsserts > (`${environment.apiRatingUrl}/role/role-assert`)
+      return this.http.get<IRoleAsserts>(`${environment.apiRatingUrl}/role/role-assert`);
     }
 
     delete(id : number) {
-        return this
-            .http
-            .delete(`${environment.apiRatingUrl}/role/${id}`)
-    }
-    createDeletePermissions(role : string, newPermission : IUpdatePermission[],) {
-        return this
-            .http
-            .put(`${environment.apiRatingUrl}/role/permission/${role}`, newPermission)
+      return this.http.delete(`${environment.apiRatingUrl}/role/${id}`);
     }
 
+    createDeletePermissions(role : string, newPermission : IUpdatePermission[]) {
+      return this.http.put(`${environment.apiRatingUrl}/role/permission/${role}`, newPermission);
+    }
 }
