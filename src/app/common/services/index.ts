c0d6eb954005e84fd626daf5d3762284b2a8ef51
ImGreatest <ImGreatest@users.noreply.github.com>
2024-05-04 15:55:29 +0300

    fix index.ts

--- src/app/common/services/index.ts
+++ src/app/common/services/index.ts
@@ -1,5 +1,3 @@
 export * from "src/app/auth/auth.service";
-export * from "src/app/common/services/api/role.service";
-export * from "src/app/common/services/api/rating.service";
-export * from "src/app/common/services/api/skill.service";
-export * from "src/app/common/services/api/user.service";
+export * from "src/app/common/services/app.service";
+export * from "src/app/common/services/api";
