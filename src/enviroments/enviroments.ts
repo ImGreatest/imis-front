deb9e4dd7a809a1ba6e7a29a0528f5ad711d9b28
Errormacr <tulupov.valeriy@mail.ru>
2024-05-06 12:02:04 +0300

    fix

--- src/enviroments/enviroments.ts
+++ src/enviroments/enviroments.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: false,
-  apiRatingUrl: 'http://localhost:3001/api',
-  // apiRatingUrl: `http://62.217.180.64:3005/api`,
+  // apiRatingUrl: 'http://localhost:3001/api',
+  apiRatingUrl: `http://62.217.180.64:3005/api`,
   //apiMobileUrl: 'http://localhost:3002/api',
   apiMobileUrl: `http://62.217.180.64:3007/api`,
   // apiUserUrl: 'http://localhost:3003/api',
-  // apiUserUrl: `http://62.217.180.64:3004/api`,
-  apiUserUrl: 'http://localhost:3000/api',
+  apiUserUrl: `http://62.217.180.64:3004/api`,
+  // apiUserUrl: 'http://localhost:3000/api',
   // apiEmployerUrl: 'http://localhost:3000/api',
   apiEmployerUrl: `http://62.217.180.64:3006/api`,
 };
