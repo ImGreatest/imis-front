ef68d0da1c4fdce3f3cf96bf4329614e3ce7dded
Deserag <pavelkopytov472@gmail.com>
2024-05-07 16:37:40 +0300

    Исправил блокироку

--- src/app/pages/company/company.component.ts
+++ src/app/pages/company/company.component.ts
@@ -4,8 +4,26 @@ import {tuiFadeIn} from "@taiga-ui/core";
 @Component({
   selector: 'app-company',
   templateUrl: './company.component.html',
-  styleUrl: './company.component.less',
+  styleUrls: ['./company.component.less'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   animations: [tuiFadeIn],
 })
-export class CompanyComponent {}
+export class CompanyComponent {
+  companies = [
+    {
+      name: 'Название компании 1',
+      description: 'Описание компании 1',
+      techStack: 'Технологии 1, Технологии 2',
+      address: 'Адрес компании 1',
+      contacts: 'Телефон, Электронная почта',
+    },
+    {
+      name: 'Название компании 2',
+      description: 'Описание компании 2',
+      techStack: 'Технологии 3, Технологии 4',
+      address: 'Адрес компании 2',
+      contacts: 'Телефон, Электронная почта',
+    }
+    // Добавьте сюда данные о дополнительных компаниях аналогичным образом
+  ];
+}
