9df73b33118b1e7ae8cf0e0883af3a655200f09b
Deserag <pavelkopytov472@gmail.com>
2024-05-07 21:29:23 +0300

    Добавил карточку компании

--- src/app/component/components.module.ts
+++ src/app/component/components.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RatingTableModule } from './rating-table/rating.module';
 import { ProjectCardModule } from './project-card/project-card.module';
+import { CompanyCardModule } from './company-card/company-card.module';
 import { AppDialogModule } from './dialog/app-dialog.module';
-
 @NgModule({
-  exports: [ProjectCardModule, RatingTableModule, AppDialogModule],
+  exports: [ProjectCardModule,CompanyCardModule, RatingTableModule, AppDialogModule],
 })
 export class ComponentsModule {}
