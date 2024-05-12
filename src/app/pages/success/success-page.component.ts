7b2c33ba86ebe8a8a41b482ced6fe439ea22f85e
ImGreatest <ImGreatest@users.noreply.github.com>
2024-05-12 22:41:16 +0300

    fix

--- src/app/pages/success/success-page.component.ts
+++ src/app/pages/success/success-page.component.ts
@@ -1,31 +1,36 @@
-import {Component, ChangeDetectionStrategy, OnInit, ChangeDetectorRef, Injector} from "@angular/core";
-import { FormGroup, FormControl, FormsModule, ReactiveFormsModule } from "@angular/forms";
-import {IFilter, ISuccess, ISuccessRes, PageRes} from "@interfaces";
-import { TuiActiveZoneModule, tuiIsFalsy, TuiLetModule } from "@taiga-ui/cdk";
 import {
-    BehaviorSubject,
-    combineLatest,
-    debounceTime,
-    map,
-    Observable,
-    share,
-    switchMap
-} from "rxjs";
-import {SuccessService} from '@services';
-import {AppDialogService} from "src/app/component/dialog/app-dialog.service";
-import {PolymorpheusComponent} from '@tinkoff/ng-polymorpheus';
-import {CreateUpdateSuccessComponent} from "./create-update-success/create-update-success.component";
-import {AuthService} from '../../auth/auth.service';
+  Component,
+  ChangeDetectionStrategy,
+  OnInit,
+  ChangeDetectorRef,
+  Injector,
+} from '@angular/core';
+import { FormGroup, FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { IFilter, ISubjectPermissions, ISuccess, ISuccessRes, PageRes } from '@interfaces';
+import { TuiActiveZoneModule, TuiLetModule } from '@taiga-ui/cdk';
+import {
+  BehaviorSubject,
+  combineLatest,
+  debounceTime,
+  map,
+  Observable,
+  share,
+  switchMap,
+} from 'rxjs';
+import { SuccessService } from '@services';
+import { AppDialogService } from 'src/app/component/dialog/app-dialog.service';
+import { PolymorpheusComponent } from '@tinkoff/ng-polymorpheus';
+import { CreateUpdateSuccessComponent } from './create-update-success/create-update-success.component';
+import { PermissionService } from '../../auth/permission.service';
 import { CreateUpdateSuccessModule } from "./create-update-success/create-update-success.module";
-import { CommonModule, NgIf } from "@angular/common";
 import { TuiInputModule, TuiPaginationModule, TuiTagModule } from "@taiga-ui/kit";
-import { TuiButtonModule, TuiLabelModule, TuiLoaderModule, TuiTextfieldControllerModule } from "@taiga-ui/core";
+import { CommonModule, NgIf } from "@angular/common";
+import { TuiButtonModule, TuiIconModule } from "@taiga-ui/experimental";
+import { TuiLabelModule, TuiLoaderModule, TuiTextfieldControllerModule } from "@taiga-ui/core";
 import { TuiTableModule } from "@taiga-ui/addon-table";
-import { TuiIconModule } from "@taiga-ui/experimental";
 
 @Component({
-  selector: 'skills',
-  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [
     CreateUpdateSuccessModule,
     FormsModule,
@@ -44,9 +49,10 @@ import { TuiIconModule } from "@taiga-ui/experimental";
     NgIf,
     TuiLetModule,
   ],
-  templateUrl: './success-page.component.html',
+  selector: 'skills',
+  standalone: true,
   styleUrl: './success-page.component.less',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  templateUrl: './success-page.component.html',
 })
 export class SuccessPageComponent implements OnInit {
   readonly search = new FormGroup({
@@ -131,7 +137,6 @@ export class SuccessPageComponent implements OnInit {
     'actions',
   ];
   success: ISuccess[] = [];
-  statusFilter = false;
   readonly size$ = new BehaviorSubject(10);
   readonly page$ = new BehaviorSubject(0);
   readonly pageCount$ = new BehaviorSubject(0);
@@ -239,7 +244,6 @@ export class SuccessPageComponent implements OnInit {
         })
       );
   }
-  readonly loading$ = this.request$.pipe(map(tuiIsFalsy));
 
   onCreateUpdateSuccess(id: number = -5): void {
     const dataToModal =
@@ -260,184 +264,9 @@ export class SuccessPageComponent implements OnInit {
           title: id < 0 ? 'Создание успеха' : 'Редактирование успеха',
           data: dataToModal,
         }
-
-        this
-            .request$
-            .subscribe((success) => {
-                this.success = success;
-                this
-                    .cdr
-                    .markForCheck()
-            });
-        this
-            .search
-            .controls
-            .searchStudentSurname
-            .valueChanges
-            .subscribe((value) => {
-                this
-                    .filters$
-                    .next(this.filters$.value.filter((filter) => filter.column !== 'user'));
-                this
-                    .filters$
-                    .next([
-                        ...this.filters$.value, {
-                            column: 'user',
-                            value: {
-                                surname: {
-                                    contains: value,
-                                    mode: 'insensitive'
-                                }
-                            }
-                        }
-                    ]);
-            });
-        this
-            .search
-            .controls
-            .searchSuccessName
-            .valueChanges
-            .subscribe((value) => {
-                console.log(value)
-                this
-                    .filters$
-                    .next(this.filters$.value.filter((filter) => filter.column !== 'name'));
-                this
-                    .filters$
-                    .next([
-                        ...this.filters$.value, {
-                            column: 'name',
-                            value: {
-                                contains: value,
-                                mode: 'insensitive'
-                            }
-                        }
-                    ]);
-            });
-    }
-
-    readonly columns = [
-        'place',
-        'name',
-        'description',
-        'tags',
-        "studentName",
-        "studentSurname",
-        "studentDirection",
-        "studentGroup",
-        "actions"
-    ];
-    success : ISuccess[] = []
-    statusFilter = false;
-    readonly size$ = new BehaviorSubject(10);
-    readonly page$ = new BehaviorSubject(0);
-    readonly pageCount$ = new BehaviorSubject(0);
-    readonly filters$ = new BehaviorSubject < IFilter[] > ([]);
-    readonly sorter$ = new BehaviorSubject < string | null > ('name');
-    readonly direction$ = new BehaviorSubject < -1 | 1 > (-1);
-
-    goToPage(index : any) : void {
-        this
-            .page$
-            .next(index);
-    }
-
-    readonly request$ = combineLatest([this.sorter$, this.direction$, this.page$, this.size$, this.filters$]).pipe(debounceTime(0.1), switchMap(([key, direction, page, size, filters]) => this.getData(key, direction, page, size, filters)), share());
-    private getData(key : string | null, direction : -1 | 1, page : number, size : number, filters : IFilter[]) : Observable < any > {
-        const sortDirection = direction === 1
-            ? 'asc'
-            : 'desc';
-        let orderProps: any = {};
-
-        if (key) {
-            switch (key) {
-                case 'studentName':
-                    orderProps = {
-                        student: {
-                            name: sortDirection
-                        }
-                    };
-                    break;
-                case 'studentSurname':
-                    orderProps = {
-                        student: {
-                            surname: sortDirection
-                        }
-                    };
-                    break;
-                case 'studentGroup':
-                    orderProps = {
-                        student: {
-                            group: {
-                                name: sortDirection
-                            }
-                        }
-                    };
-                    break;
-                case 'studentDirection':
-                    orderProps = {
-                        student: {
-                            direction: {
-                                name: sortDirection
-                            }
-                        }
-                    };
-                    break;
-                default:
-                    orderProps = {
-                        [key]: sortDirection
-                    };
-                    break;
-            }
-        }
-        return this
-            .successService
-            .getSuccessPage({
-                orderProps: orderProps,
-                page: page + 1,
-                pageSize: size,
-                filters: filters
-            })
-            .pipe(map((data : PageRes < ISuccessRes >) => {
-                this
-                    .pageCount$
-                    .next(data.info.totalPages);
-                return data
-                    .rows
-                    .map((success : ISuccessRes) => ({
-                        id: success.id,
-                        name: success.name,
-                        description: success.description,
-                        tags: success
-                            .tags
-                            .map((tag) => tag.tag),
-                        studentName: success.student.name,
-                        studentSurname: success.student.surname,
-                        studentDirection: success.student.direction.name,
-                        studentGroup: success.student.group.name
-                    }));
-            }));
-    }
-    readonly loading$ = this
-        .request$
-        .pipe(map(tuiIsFalsy));
-
-    onCreateUpdateSuccess(id : number = -5) : void {
-        const dataToModal = id < 0
-            ? {}
-            : {
-                successId: id
-            }
-            this
-                .appDialogService
-                .open(new PolymorpheusComponent < any, any > (CreateUpdateSuccessComponent, this.injector), {
-                    size: 'm',
-                    closeable: true,
-                    title: id < 0
-                        ? 'Создание успеха'
-                        : 'Редактирование успеха',
-                    data: dataToModal
-                })
-                .subscribe((value : any) => value === 'created' && this.page$.next(this.page$.value));
-    }
+      )
+      .subscribe(
+        (value: any) => value === 'created' && this.page$.next(this.page$.value)
+      );
+  }
 }
